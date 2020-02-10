a83770b6ef1484b8358b2f6278489ecbdab239c7
Yuzunoha <studysteadystandby@gmail.com>
2020-02-10 17:33:03 +0900

    サインアップを実装した

--- main.js
+++ main.js
@@ -6,5 +6,25 @@ const onclick_sign_up = () => {
   const email = document.getElementById('sign_up_email').value;
   const password = document.getElementById('sign_up_password').value;
   const password_confirmation = document.getElementById('sign_up_password_confirmation').value;
-  alert("onclick_sign_up()が呼ばれました" + name + bio + email + password + password_confirmation);
+  const url = 'https://teachapi.herokuapp.com/sign_up';
+  const data = {
+    sign_up_user_params: {
+      name,
+      bio,
+      email,
+      password,
+      password_confirmation,
+    }
+  };
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+    });
 };
