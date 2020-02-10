4421c90e0ec3879d7907120e66c43cd2e8eb0b7a
Yuzunoha <studysteadystandby@gmail.com>
2020-02-10 17:25:56 +0900

    値を受け取れるようにした

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 'use strict';
 
 const onclick_sign_up = () => {
-  alert("onclick_sign_up()が呼ばれました");
+  const name = document.getElementById('sign_up_name').value;
+  const bio = document.getElementById('sign_up_bio').value;
+  const email = document.getElementById('sign_up_email').value;
+  const password = document.getElementById('sign_up_password').value;
+  const password_confirmation = document.getElementById('sign_up_password_confirmation').value;
+  alert("onclick_sign_up()が呼ばれました" + name + bio + email + password + password_confirmation);
 };
